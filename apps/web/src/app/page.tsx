b3d505f26200e495d8fd60eea4a36e21ffae0e1f
James Hoffmann <dragon99steel@gmail.com>
2024-04-09 16:48:55 -0400

    ref: added catch block for first page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -19,14 +19,14 @@ const apiTest = async () => {
 export const dynamic = 'force-dynamic'
 
 export default async function Home() {
-  const workspace = await apiTest()
+  const workspace = await apiTest().catch((e)=>undefined)
   return (
     <DefaultLayoutComponent>
-      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
+      {workspace ? <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex items-center justify-center w-full h-48 gap-2 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           {workspace.name}{" "}{workspace.version}
         </div>
-      </div>
+      </div> : <></>}
 
       <div className="relative flex flex-1 self-center place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-amber-200 after:via-primary-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-primary-700 before:dark:opacity-10 after:dark:from-amber-900 after:dark:via-primary after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
